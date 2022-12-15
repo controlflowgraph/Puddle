3bea02babe1e5e26ca33667788a90d3058db8385
ControlFlow Graph <controlflowgraph@gmail.com>
2022-12-16 00:18:05 +0100

    add index assignment

--- src/puddle/ast/segment/Index.java
+++ src/puddle/ast/segment/Index.java
@@ -17,7 +17,6 @@ public record Index(Segment source, Segment index) implements Segment
 		if(s instanceof List l)
 		{
 			int value = (int)(double)(Double) this.index.eval(c);
-			System.out.println(l);
 			return l.get(value);
 		}
 		if(s instanceof Map m)
@@ -26,5 +25,30 @@ public record Index(Segment source, Segment index) implements Segment
 			return m.get(k);
 		}
 		throw new RuntimeException("Unable to index " + s.getClass());
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void assign(Context c, Object v)
+	{
+		Object s = this.source.eval(c);
+		if(s instanceof Object[] a)
+                {
+                        int value = (int)(double)(Double) this.index.eval(c);
+                        a[value] = v;
+                }
+                else if(s instanceof List)
+                {
+                	List<Object> l = (List<Object>) s;
+                        int value = (int)(double)(Double) this.index.eval(c);
+                        l.set(value, v);
+                }
+                else if(s instanceof Map)
+                {
+                	Map<Object, Object> m = (Map<Object, Object>) s;
+                        Object k = this.index.eval(c);
+                        m.put(k, v);
+                }
+                else throw new RuntimeException("Unable to index " + s.getClass());
 	}
 }
