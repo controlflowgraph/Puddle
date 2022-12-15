011572ab573f14e5d630b28d9d171121940d9cce
ControlFlow Graph <controlflowgraph@gmail.com>
2022-12-15 12:09:40 +0100

    add toLines method

--- src/puddle/Puddle.java
+++ src/puddle/Puddle.java
@@ -1,8 +1,38 @@
+import java.util.*;
+import java.nio.file.*;
+
 public class Puddle
 {
 	public static void main(String[] args)
 	{
-		print("Puddle Lang");
+		var text = read("./data/test.pdl");
+		var lines = toLines(text);
+		print(lines);
+	}
+
+	private static List<Line> toLines(String text)
+	{
+		return Lexer.tokenize(text)
+			.stream()
+			.filter(l -> !l.tokens().isEmpty())
+			.toList();
+	}
+
+	private static String read(String path)
+	{
+		try
+		{
+			return Files.readString(Path.of(path));
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void print(List<?> lst)
+	{
+		lst.forEach(Puddle::print);
 	}
 
 	private static void print(Object o)
